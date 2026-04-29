6401da7c4739e5a9be8a783e0c669377603b004a
OmniStage <omnistageproducoes@gmail.com>
2026-04-29 00:54:44 -0300

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,7 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
+
 import { Header } from "@/components/Header";
 import { supabase } from "@/lib/supabase";
 
