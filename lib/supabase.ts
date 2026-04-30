aa4f65160f51afa001825e2125ee0c8b4778182a
OmniStage <omnistageproducoes@gmail.com>
2026-04-30 00:35:34 -0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,13 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-console.log("URL:", supabaseUrl);
-console.log("KEY:", supabaseAnonKey?.slice(0, 10));
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  global: {
-    fetch: fetch,
-  },
-});
+export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey);
