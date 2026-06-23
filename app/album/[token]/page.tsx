934447dfaababdc51a9bface7b9862c54573b224
VITOR JOSÉ <vitorjose@MacBook-Pro-de-VITOR.local>
2026-06-22 22:40:30 -0300

    fix: featured photo aspect ratio 9/16 (vertical)

--- app/album/[token]/page.tsx
+++ app/album/[token]/page.tsx
@@ -841,7 +841,7 @@ function DestaqueCarousel({ lista, idx, onIdxChange, curtidas, onCurtir }: {
           scrollbarWidth: "none" as any,
           msOverflowStyle: "none" as any,
           width: "100%",
-          aspectRatio: "3/4",
+          aspectRatio: "9/16",
           background: "#111",
           borderRadius: 12,
         } as React.CSSProperties}
